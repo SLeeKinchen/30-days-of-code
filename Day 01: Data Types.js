d94926fdd75c55c4f4b4422ac1f1730cd01da450
Sydney Lee Kinchen <37483124+SLeeKinchen@users.noreply.github.com>
2019-05-21 15:52:45 -0400

    Create Day 01: Data Types.js

---
+++ Day 01: Data Types.js
@@ -0,0 +1,20 @@
+// Declare second integer, double, and String variables.
+    var a = 12;
+    var b = 4.0;
+    var c = "is the best place to learn and practice coding!";   
+    
+// Read and save an integer, double, and String to your variables.
+    a = parseInt(readLine());
+    b = parseFloat(readLine());
+    c = readLine();
+    
+// Print the sum of both integer variables on a new line. 
+    console.log(i + a);
+    
+// Print the sum of the double variables on a new line.
+    console.log((d + b).toFixed(1));
+    
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+    console.log(s + c);   
+}
